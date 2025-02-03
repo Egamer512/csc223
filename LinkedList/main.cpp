e832789dedd03531c22eb2bd7bef2285bf4b1f4e
Anfal <Egamer8544@gmail.com>
2025-02-03 08:54:59 -0500

    working on the chapter

---
+++ LinkedList/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int cargo;
+    Node* next;
+
+    Node() {
+        cargo = 0;
+        next = nullptr;
+    }
+
+    Node(int cargo) {
+        this->cargo = cargo;
+        next = nullptr;
+    }
+
+    Node(int cargo, Node* next) {
+        this->cargo = cargo;
+        this->next = next;
+    }
+
+    string to_str() const {
+        return to_string(cargo);
+    }
+};
