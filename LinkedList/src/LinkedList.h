9ae77e73ae3dd217b53e815f00ea9658867ff6de
Anfal <Egamer8544@gmail.com>
2025-02-06 17:41:46 -0500

    started on the temaplte for Node and constructor

--- LinkedList/src/LinkedList.h
+++ LinkedList/src/LinkedList.h
@@ -1,3 +1,15 @@
 #include <iostream>
 using namespace std;
 
+template <class T>
+class Node{
+    T cargo;
+    Node<T>* next;
+
+    public:
+        Node(T cargo, Node<T>* next){
+            this->cargo = cargo;
+            this->next = next;
+        }
+};
+
