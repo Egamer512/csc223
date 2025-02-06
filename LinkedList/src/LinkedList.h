7c7819b424d26500f1c694bc9c6841e9869aa7bf
Anfal <Egamer8544@gmail.com>
2025-02-06 17:40:13 -0500

    working on the class and templates, goodbye structs :(

--- LinkedList/src/LinkedList.h
+++ LinkedList/src/LinkedList.h
@@ -1,20 +1,3 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
-    int cargo;
-    Node* next;
-
-    Node();
-    Node(int);
-    Node(int, Node*);
-
-    string to_str() const;
-
-};
-
-string render_list(Node* list);
-
-string render_list_backward(Node* list);
-
-string render_pretty(Node* list, string (*list_renderer)(Node*));
