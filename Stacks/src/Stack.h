bda00652aff5fb82082f29dea0390a5f8c19a7e2
Anfal <Egamer8544@gmail.com>
2025-02-28 08:47:58 -0500

    adedd the simple member functions

--- Stacks/src/Stack.h
+++ Stacks/src/Stack.h
@@ -3,4 +3,22 @@ using namespace std;
 #include "LinkedList.h"
 
 template <class T>
-class Stack
+class Stack{
+    LinkedList<T> items;
+    public:
+    void push(T item){
+        items.insert_at_front(item);
+    }
+    T pop() {
+        if (is_empty()){
+            throw runtime_error("Can't remove an empty list(Stack)!")
+        }
+        return items.remove_from_front();
+    }
+    bool is_empty() const{
+        return items.length() == 0;
+    }
+    T top(){
+        ig
+    }
+};
