7d74268f02266d5784156903937bc810d3c3440c
Anfal <Egamer8544@gmail.com>
2025-03-16 01:43:44 -0400

    removed the old function and modified hte new function

--- Queues/src/Queue.h
+++ Queues/src/Queue.h
@@ -10,8 +10,13 @@ class Queue : public LinkedList<T>
     // modifiers
     void insert(T item) {
        if(LinkedList<T>::length() == 0){
-        LinkedList<T>::insert_at_front(item);
+        LinkedList<T>::insert_at_front(item); // adding for an empty queue
        }
+       int i = 1; // looping to check whether or not item is less than an element (basically, we want to loop until the next max)
+       while(i <= LinkedList<T>::length() && item < LinkedList<T> get_item_at(i)){
+        i++;
+       }
+       LinkedList<T>::insert_item_at(item, i -1);
     }
 
     T remove() {
@@ -21,15 +26,5 @@ class Queue : public LinkedList<T>
     bool empty() {
         return (LinkedList<T>::length() == 0);
     }
-
-    void insert_in_order(T item){
-        for(int i = 0; i < LinkedList<T>::length(); i++){
-            T element = LinkedList<T>::get_item_at(i);
-            if(item < element){
-                LinkedList<T>::insert_item_at(i);
-            }
-        }
-    }
-
 
 };
