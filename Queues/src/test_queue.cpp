8ccb5401c1c564e0f0042281ddd158aff10b3177
Anfal <Egamer8544@gmail.com>
2025-03-18 20:07:46 -0400

    added the new tests

--- Queues/src/test_queue.cpp
+++ Queues/src/test_queue.cpp
@@ -4,37 +4,68 @@
 #include "Queue.h"
 using namespace std;
 
-/* TEST_CASE("Test basic queue operations on queue of strings") {
-    Queue<string> queue;
-    CHECK(queue.empty() == true);
-    queue.insert("first");
-    CHECK(queue.empty() == false);
-    queue.insert("second");
-    queue.insert("third");
-    CHECK(queue.remove() == "first");
-    queue.insert("forth");
-    queue.insert("fifth");
-    queue.insert("sixth");
-    queue.insert("seventh");
-    queue.insert("eight");
-    queue.insert("nineth");
-    CHECK(queue.remove() == "second");
-    CHECK(queue.remove() == "third");
-    queue.insert("tenth");
-    queue.insert("eleventh");
-    CHECK(queue.remove() == "forth");
-    queue.insert("twelfth");
-    queue.insert("thirteenth");
-    CHECK(queue.remove() == "fifth");
+TEST_CASE("Test can create empty PriorityQueue") {
+    Queue<string> pq;
+    CHECK(pq.empty() == true);
 }
-*/
-
-TEST_CASE("looking to see if my function actually kinda works"){
-    Queue<int> queue;
-    queue.insert(5);
-    queue.insert(3);
-    queue.insert(7);
-    CHECK(queue.remove() == 7);
-    CHECK(queue.remove() == 5);
-    CHECK(queue.remove() == 3);
+TEST_CASE("Test can insert and remove items in PriorityQueue") {
+    Queue<int> pq;
+    pq.insert(10);
+    CHECK(pq.empty() == false);
+    CHECK(pq.remove() == 10);
+    CHECK(pq.empty() == true);
+    pq.insert(12);
+    pq.insert(8);
+    CHECK(pq.remove() == 12);
+    CHECK(pq.remove() == 8);
+    CHECK(pq.empty() == true);
+    pq.insert(9);
+    pq.insert(6);
+    pq.insert(7);
+    CHECK(pq.remove() == 9);
+    CHECK(pq.remove() == 7);
+    CHECK(pq.remove() == 6);
+    CHECK(pq.empty() == true);
+    pq.insert(14);
+    pq.insert(11);
+    pq.insert(42);
+    CHECK(pq.remove() == 42);
+    CHECK(pq.remove() == 14);
+    CHECK(pq.remove() == 11);
+    CHECK(pq.empty() == true);
+    pq.insert(14);
+    pq.insert(11);
+    pq.insert(42);
+    pq.insert(6);
+    CHECK(pq.remove() == 42);
+    CHECK(pq.remove() == 14);
+    CHECK(pq.remove() == 11);
+    CHECK(pq.remove() == 6);
+    CHECK(pq.empty() == true);
+    pq.insert(14);
+    pq.insert(11);
+    pq.insert(42);
+    pq.insert(6);
+    pq.insert(16);
+    CHECK(pq.remove() == 42);
+    CHECK(pq.remove() == 16);
+    CHECK(pq.remove() == 14);
+    CHECK(pq.remove() == 11);
+    CHECK(pq.remove() == 6);
+    CHECK(pq.empty() == true);
+    pq.insert(14);
+    pq.insert(11);
+    pq.insert(42);
+    pq.insert(6);
+    pq.insert(16);
+    pq.insert(8);
+    pq.insert(2);
+    CHECK(pq.remove() == 42);
+    CHECK(pq.remove() == 16);
+    CHECK(pq.remove() == 14);
+    CHECK(pq.remove() == 11);
+    CHECK(pq.remove() == 8);
+    CHECK(pq.remove() == 6);
+    CHECK(pq.remove() == 2);
+    CHECK(pq.empty() == true);
 }
