33ab060a6903956091f0976e2cbc2b11019cb68e
Anfal <Egamer8544@gmail.com>
2025-03-24 23:43:16 -0400

    starting with the doctest

---
+++ Queues/src/test_RGTPQ.cpp
@@ -0,0 +1,18 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+#include <string>
+#include "RGTPQ.h"
+using namespace std;
+
+TEST_CASE("Insertion and Removal"){
+    RGTPQ Queue;
+
+    RGThing red1 {RED};
+    RGThing red2 {RED};
+    RGThing green1 {GREEN};
+    RGThing red3 {RED};
+    RGThing green2 {GREEN};
+    
+
+
+}
