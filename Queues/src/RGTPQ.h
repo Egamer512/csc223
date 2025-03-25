12083c77196ac98445c7c983b1a476be66a1d719
Anfal <Egamer8544@gmail.com>
2025-03-24 23:28:35 -0400

    added the two structs

--- Queues/src/RGTPQ.h
+++ Queues/src/RGTPQ.h
@@ -1,1 +1,17 @@
 #include "NormalQueue.h"
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+enum Color {RED, GREEN}; // Our two colors
+
+struct RGThing {
+    Color color;
+    string label;
+};
+
+struct RGTpair {
+    RGThing item1;
+    RGThing item2;
+};
