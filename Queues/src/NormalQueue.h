4a8e329d84688b19ee19b724a199150f46887722
Anfal <Egamer8544@gmail.com>
2025-03-24 23:23:56 -0400

    starting on making the FIFO Queue

---
+++ Queues/src/NormalQueue.h
@@ -0,0 +1,25 @@
+#include "LinkedList.h"
+
+template <class T>
+class NormalQueue : public LinkedList<T>
+{
+  public:
+    // constructors
+    Queue() = default;
+
+    // modifiers
+    void insert(T item) {
+        LinkedList<T>::insert_at_end(item);
+    }
+
+    T remove() {
+        if(empty()){
+            throw std::underflow_error("can't remove from an empty queue");
+        }
+        return LinkedList<T>::remove_from_front();
+    }
+
+    bool empty() {
+        return (LinkedList<T>::length() == 0);
+    }
+};
