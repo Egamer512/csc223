8b4d9386f24f2c8a8146f9b28b3ec4d412690351
Anfal <Egamer8544@gmail.com>
2025-05-19 22:17:25 -0400

    start on merge sort psuedo code

---
+++ Ruby/MergeSort.rb
@@ -0,0 +1,12 @@
+def merge_sort(array)
+  return array if array.length <= 1
+
+  mid = array.length / 2
+  left = merge_sort(array[0...mid])
+  right = merge_sort(array[mid...array.length])
+
+  merge(left, right)
+
+end
+
+
